958fa0847629a3b92a88e63ae2180e1dd5b39c5f
cleitson <cleitson.ftw@gmail.com>
2023-07-11 20:44:22 -0300

    requisito 5 completo

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -17,6 +17,8 @@ function Form({ handleShow }: FormProp) {
     password: '',
     url: '',
   };
+  const invalidClass = 'invalid-password-check';
+  const validClass = 'valid-password-check';
 
   const [formData, setFormData] = useState<InitialStateProp>(initialState);
   const [buttonDisable, setButtonDisable] = useState<boolean>(true);
@@ -28,21 +30,26 @@ function Form({ handleShow }: FormProp) {
       ...formData,
       [name]: value,
     });
-    isValid();
+    isValidForm();
   }
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setFormData(initialState);
+    console.log(formData);
   };
 
   const { service, login, password, url } = formData;
 
   const senhaRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,16}$/;
-
   const isValidPassword: boolean = senhaRegex.test(password);
 
-  function isValid() {
+  const symbolRegex = /^(?=.*[@$!%*#?&])/;
+  const validSymbol:boolean = symbolRegex.test(password);
+  const letterNumberRegex = /^(?=.*[a-zA-Z])(?=.*\d)/;
+  const validLetterNumeber:boolean = letterNumberRegex.test(password);
+
+  function isValidForm() {
     if (service.length > 0 && login.length > 0 && isValidPassword) {
       setButtonDisable(false);
     }
@@ -93,6 +100,23 @@ function Form({ handleShow }: FormProp) {
           onChange={ handleChange }
         />
       </label>
+      <div>
+        <ul>
+          <p>a senha deve conter:</p>
+          <li className={ (password.length < 8) ? invalidClass : validClass }>
+            Possuir 8 ou mais caracteres
+          </li>
+          <li className={ (password.length > 16) ? invalidClass : validClass }>
+            Possuir até 16 caracteres
+          </li>
+          <li className={ (!validLetterNumeber) ? invalidClass : validClass }>
+            Possuir letras e números
+          </li>
+          <li className={ (!validSymbol) ? invalidClass : validClass }>
+            Possuir algum caractere especial
+          </li>
+        </ul>
+      </div>
       <button disabled={ buttonDisable }>Cadastrar</button>
       <button onClick={ handleShow }>Cancelar</button>
     </form>
